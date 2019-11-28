28686dbe2833187015279c16c8791802b5608a08
Moritz Weiss <moritz.weiss95@hotmail.de>
2019-11-28 13:33:07 +1100

    some tests besides the tutorial

--- movie-mojo/src/components/App.js
+++ movie-mojo/src/components/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Collection from './Collection';
 import '../App.css';
 
 class App extends Component {
   render() {
-    const headerText = "Moe's Movie Mojo App"
+    const headerText = "Moritz' App"
     return (
       <div className="App">
         <Header text={headerText} />
         <p className="App-intro">
           Welcome to the 'Movie Mojo' React app!
         </p>
+        <Collection />
       </div>
     );
   }
---
+++ movie-mojo/src/components/Collection.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Collection extends Component {
+    render() {
+        return (
+            <div className="collectionWrapper">
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+                <div className="collection">
+                    <img className="previewImage" src="https://cdn.pixabay.com/photo/2019/11/06/14/33/moon-4606246_1280.jpg"></img>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Collection
